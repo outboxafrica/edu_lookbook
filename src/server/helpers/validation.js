ee0f6d44822bc56d584c69229590dc047e870fc2
makwasi1 <crismakwasi12@gmail.com>
2020-06-11 20:11:15 +0300

    ch(add: validation with @hapi/joi)

--- src/server/helpers/validation.js
+++ src/server/helpers/validation.js
@@ -1,53 +1,26 @@
+module.exports = { 
+  validateBody: (schema) => {
+    return (req, res, next) => {
+      const result = schema.validate(req.body);
+      if (result.error) {
+        res.status(403).json({ Error: result.error.details[0].message });
+      } else {
+        next();
+      }
+    };
+  },
 
-//import the Validation Schemas
-const { userIdValidSchema } = require('./schemas/profile');
-const { profileSchema, profileIdSchema, optionalSchema } = require('./validationSchemas');
-
-
-module.exports = {
-	validation       : (schema) => {
-		return (req, res, next) => {
-			const result = schema.validate(req.body);
-			if (result.error) {
-				res.status(403).json({ Message: 'Something went wrong', Error: result.error.details[0].message });
-			} else {
-				next();
-			}
-		};
-	},
-	userIdValidation : (req, res, next) => {
-		const result = userIdValidSchema.validate(req.params);
-		if (result.error) {
-			//throw an error
-			res.status(403).json({ Message: 'Something Went Wrong', Error: result.error.details[0].message })
-
-		} },
-
-	profileValidator   : (req, res, next) => {
-		const result = profileSchema.validate(req.body);
-		if (result.error) {
-			res.status(403).json({Error:result.error.details[0].message});
-		} else {
-			next();
-		}
-	},
-	updateValidator    : (req, res, next) => {
-		const result =  optionalSchema.validate(req.body);
-		if (result.error) {
-			res.status(403).json({Error:result.error.details[0].message});
-		} else {
-			next();
-		}
-	},
-	profileIdValidator : (req, res, next) => {
-		const result = profileIdSchema.validate(req.params);
-		if (result.error) {
-			res.status(403).json(result.error.details[0].message);
-
-			res.status(403).json({Message:'Invalid profile ID provided',Error:result.error.details[0].message});
-
-		} else {
-			next();
-		}
-	}
-}
+  validateParams: (schema) => {
+    return (req, res, next) => {
+      const result = schema.validate(req.params);
+      if (result.error) {
+        res.status(403).json({
+          Message: "Invalid profile ID provided",
+          Error: result.error.details[0].message,
+        });
+      } else { 
+        next();
+      }
+    };
+  },
+};
