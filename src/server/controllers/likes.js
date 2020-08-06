944d373c6baa989eaa994ff1a42e215fb1dfda53
ShNorah <aksharon018@gmail.com>
2020-08-06 11:25:26 +0300

    ft(LikeProfie):modify profile controller

--- src/server/controllers/likes.js
+++ src/server/controllers/likes.js
@@ -5,13 +5,12 @@ const StatusCodes = require('http-status-codes');
 module.exports = {
   checkProfile: async (req, res) => {
     try {
-      req.body.username = req.username
-      const user = await Profile.findOne({ username: req.username });
-      if (user) {
+      const profile = await Profile.findOne({ _id: req.body.profile_id });  
+      if (profile) {
         const NewLike = new Like(req.body);
         const savedLike = await NewLike.save();
         res.status(StatusCodes.OK).json({
-          Message: "You have liked this user",
+          Message: "You have liked this user profile",
           Like: savedLike,
         });
       } else {
@@ -21,30 +20,26 @@ module.exports = {
       }
     
     } catch (err) {
-      console.log("error while liking this user", err)
+      console.log("error while liking this user profile", err)
       res.status(StatusCodes.FORBIDDEN)
-      .json({error: "failed to like this user"})
+      .json({error: "failed to like this user profile"})
     }
   },
   authenticate: async (req, res) => {
     try {
-      const user = await Profile.findById(req.params.user_id);
+      const profile = await Profile.findById(req.params.profile_id);
       //check if the ids are the same
-      if (req.user.id === req.params.user_id) {
+      if (req.profile_id === req.params.profile_id) {
         return res.status(StatusCodes.FORBIDDEN).json({error: "you can not like yourself"})
       
-        //check if the requested user is already in the likes list
-        if (
-          user.Likes.filter(
-            (profileLike) => profileLike.user.toString() === req.user.id
-          ).length > 0
-        ) {
-          return res.status(400).json({ message: "you already liked this profile" });
+        //check if a like  is already in the likes array
+        const existingLike = profile.find({ likes: { id: req.params._id } });
+        if (existingLike) {
+          return res.status(StatusCodes.FORBIDDEN).json({error: "you have already liked this profile"})
         } else {
-          user.Likes.unshift({ user: req.user.id });
-          user.save();
-        
+          return res.status(StatusCodes.ACCEPTED).json({message: "you have not yet liked this profile"})     
         }
+       
       }
     } catch (err) {
       res.status(StatusCodes.CONFLICT).json({error: "you can not like this profile"})
--- src/server/routers/likesRoute.js
+++ src/server/routers/likesRoute.js
@@ -3,6 +3,8 @@ const router = require('express-promise-router')();
 //import the likes controller
 const { checkProfile, authenticate } = require('../controllers/likes')
 
-router.route('/:user_id').post(checkProfile, authenticate)
+router.route('/create').post(checkProfile)
+
+router.route("/:profile_id").get(authenticate);
 
 module.exports = router;
