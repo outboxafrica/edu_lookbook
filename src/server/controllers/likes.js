b15f9422ad556ec42608e03bcc9c4a15b7db5fff
ShNorah <aksharon018@gmail.com>
2020-09-03 15:50:38 +0300

    rf(code clean up)

--- src/server/controllers/likes.js
+++ src/server/controllers/likes.js
@@ -6,18 +6,30 @@ module.exports = {
   checkProfile: async (req, res) => {
     try {
       const profile = await Profile.findById(req.body.profile_id);
-      if (profile) {
+       if (!profile) {
+         return res
+           .status(StatusCodes.CONFLICT)
+           .json({ Error: "this userprofile does not exist" });
+      }
+      console.log(req.userID)
+    
+      if (req.userID === profile.user_id) {
+        return "you can not like yourself";
+      }
+      else {
         const NewLike = new Like(req.body);
         const savedLike = await NewLike.save();
-        console.log(savedLike);
+        const updateProfile = await Profile.updateOne(
+          { _id: profile.userID },
+          { $push: { likes: savedLike } }
+        );
+        
         res.status(StatusCodes.OK).json({
           Message: "You have liked this user profile",
           Like: savedLike,
         });
-      } else {
-        res
-          .status(StatusCodes.CONFLICT)
-          .json({ Error: "this userprofile does not exist" });
+        console.log(savedLike);
+        console.log(updateProfile);
       }
     } catch (err) {
       console.log("error while liking this user profile", err);
@@ -26,46 +38,5 @@ module.exports = {
         .json({ error: "failed to like this user profile" });
     }
   },
-  authenticate: async (req, res) => {
-    try {
-      const profile = await Profile.findById(req.params.id);
-      //check if the ids are the same
-      if (req.body.profile_id === req.params.id) {
-        return res
-          .status(StatusCodes.FORBIDDEN)
-          .json({ error: "you can not like yourself" });
-      }
 
-      //check if a like  is already in the likes array
-      const existingLike = profile.find({ likes: { id: req.params._id } });
-      if (existingLike) {
-        return res
-          .status(StatusCodes.FORBIDDEN)
-          .json({ error: "you have already liked this profile" });
-      } else {
-        return res
-          .status(StatusCodes.ACCEPTED)
-          .json({ message: "you have not yet liked this profile" });
-      }
-    } catch (err) {
-      res
-        .status(StatusCodes.CONFLICT)
-        .json({ error: "you can not like this profile" });
-    }
-    next();
-  },
-
-  update: async (req, res) => {
-    try {
-      const updateProfile = await Profile.updateOne(
-        { _id: req.params.id },
-        { $push: { likes: savedLike } }
-      );
-      console.log(updateProfile);
-    } catch (err) {
-      res
-        .status(StatusCodes.EXPECTATION_FAILED)
-        .json({ error: "failed to update this profile" });
-    }
-  },
 };
--- src/server/routers/likesRoute.js
+++ src/server/routers/likesRoute.js
@@ -1,11 +1,8 @@
 const router = require('express-promise-router')();
 
 //import the likes controller
-const { checkProfile, authenticate, update } = require('../controllers/likes')
+const { checkProfile} = require('../controllers/likes')
 
-router.route('/create').post(checkProfile, authenticate, update);
-
-
-//router.route("/:profile_id").get();
+router.route('/create').post(checkProfile);
 
 module.exports = router;
