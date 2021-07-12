088bd927b5a3211d53a2d74ae3dfc189fcc3650c
Kallyas <kallyasmedia@gmail.com>
2021-07-12 14:17:45 +0300

    ft:(add helmet for security)

--- src/server/helpers/rateLimit.js
+++ src/server/helpers/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit")
 
 const rateLimiter = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, //24 hrs in Milliseconds
-    max: 1000, //number of API calls that can be made within 24 hrs
+    max: 5, //number of API calls that can be made within 24 hrs
     message: {
         error: "too many requests",
         message: "Too many requests from this IP, please try again later"
